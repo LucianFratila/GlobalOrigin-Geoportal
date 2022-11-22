f2e686d00b856d46f874b9fa64ab4cb77fccc3f7
bogdflor <bogdan@zenithmaps.com>
2022-11-22 19:35:27 +0200

    modified:   src/harvesting/HarvestingLayers.js 	modified:   src/harvesting/HarvestingPage.js

--- src/harvesting/HarvestingLayers.js
+++ src/harvesting/HarvestingLayers.js
@@ -1,15 +1,41 @@
-import React from 'react'
-import HarvestingLayers from './HarvestingLayers'
+import {useEffect,useCallback} from 'react'
+import {addVectorLayer,removeVectorLayer} from 'map/VectorLayer'
+import axios from "axios"
 
 
 export default function HarvestingLayers({map, mapLoaded}){
+    const getData = useCallback(
+        () => {
+            const bounds = map.current.getBounds();
+            axios.get(`http://hrttrt.zenithmaps.com/drumuri/vectors?bbox=${bounds.getWest()},${bounds.getSouth()},${bounds.getEast()},${bounds.getNorth()}&resolution=5`)
+                .then(response => {
+                    map.current.getSource('drumuri').setData(response.data);
+                }) 
+        },[]);
+
+    useEffect(()=>{
+        if(mapLoaded){
+            addVectorLayer(map.current,'drumuri','line',getData)
+        }
+
+    },[mapLoaded])
+
+    useEffect(()=>{
+
+        return () => {
+            //alert('unmount marcaje')
+            removeVectorLayer(map.current,'drumuri',getData)
+        }
+
+    },[])
+
 
     return(
-        <React.Fragment>
-            <div><h5 className="text-muted">Harvesting PAGE</h5></div>
-        {/*  <Legend>   de facut legenda  */}
-            <HarvestingLayers map={map} mapLoaded={mapLoaded} ></HarvestingLayers>
-        {/* </Legend> */}
-        </React.Fragment>    
+
+        <div></div>
     )
+
+
+
+
 }
--- src/harvesting/HarvestingPage.js
+++ src/harvesting/HarvestingPage.js
@@ -1,36 +1,15 @@
 
-import {useEffect,useCallback} from 'react'
-import {addVectorLayer,removeVectorLayer} from 'map/VectorLayer'
-import axios from "axios"
+import React from 'react'
+import HarvestingLayers from './HarvestingLayers'
 
 export default function HarvestingPage({map,mapLoaded}){
 
-    const getData = useCallback(
-        () => {
-            const bounds = map.current.getBounds();
-            axios.get(`http://hrttrt.zenithmaps.com/drumuri/vectors?bbox=${bounds.getWest()},${bounds.getSouth()},${bounds.getEast()},${bounds.getNorth()}&resolution=5`)
-                .then(response => {
-                    map.current.getSource('drumuri').setData(response.data);
-                }) 
-        },[]);
-
-    useEffect(()=>{
-        if(mapLoaded){
-            addVectorLayer(map.current,'drumuri','line',getData)
-        }
-
-    },[mapLoaded])
-
-    useEffect(()=>{
-        
-        return () => {
-            //
-            removeVectorLayer(map.current,'drumuri',getData)
-        }
-
-    },[])
-
     return(
-        <div><h5 className="text-muted">HARVESTING PAGE</h5></div>
+        <React.Fragment>
+            <div><h5 className="text-muted">Harvesting PAGE</h5></div>
+        {/*  <Legend>   de facut legenda  */}
+            <HarvestingLayers map={map} mapLoaded={mapLoaded} ></HarvestingLayers>
+        {/* </Legend> */}
+        </React.Fragment>    
     )
 }
