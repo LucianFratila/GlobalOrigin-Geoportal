c1880daa81797bed5fd165e92314995cd6bac039
Lucian Fratila <sirtitish@gmail.com>
2022-11-23 19:20:37 +0200

    legenda

---
+++ src/concessions/ConcessionMapLegend.js
@@ -0,0 +1,7 @@
+const ConcessionMapLegend = ({ children }) => {
+  return <>
+  <div className=" w-56 h-56 bg-slate-900 absolute right-0 top-20 z-60">{children}</div>
+  </>;
+};
+
+export default ConcessionMapLegend;
