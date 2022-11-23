c044967884d177d393e8b627c1a2e3e852cb5602
bogdflor <bogdan@zenithmaps.com>
2022-11-23 18:31:18 +0200

    modified:   src/App.css 	modified:   src/concessions/ConcessionsPage.js

--- src/concessions/ConcessionsPage.js
+++ src/concessions/ConcessionsPage.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import ConcessionsLayers from './ConcessionsLayers'
-import Legend from 'map/Legend'
-
-export default function ConcessionsPage({ map, mapLoaded}){
-    
-    let layersProps={
-        'concessions':{
-            'visibility':'visible'
-        },
-        'ufa':{
-            'visibility':'none'
-        },
-        'ufg':{
-            'visibility':'none'
-        },
-        'aac':{
-            'visibility':'visible'
-        }
-    }
-   
-    return(
-        <React.Fragment>
-            <div><h5 className="text-muted">CONSESSIONS PAGE</h5></div>
-            <Legend>
-                <ConcessionsLayers map={map} mapLoaded={mapLoaded} layersProps={layersProps} ></ConcessionsLayers>
-            </Legend>
-        </React.Fragment>    
-    )
-}
-=======
-
 import React from "react";
 
 import ToggleCheckBox from "components/reusable/toggleCheckbox";
@@ -174,4 +141,3 @@ export default function ConcessionsPage({ map, mapLoaded }) {
   );
 }
 
->>>>>>> 8dc807677cbcbaa783a99e5fdd4c32b1e84a4bc8
